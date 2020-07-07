a98abb7bc7532a62e8543a164885aebacd1af5a5
Bryce Walsh <bryce.j.walsh@gmail.com>
2020-07-07 13:50:13 -0400

    add textbox which plays notes based on keystrokes only when the key is not already pressed

---
+++ typeSound.js
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>TypeSound</title>
+		<meta charset="utf-8">
+	</head>
+	<body>	
+		<script type="text/javascript">
+			if (navigator.requestMIDIAccess) {
+			    console.log('This browser supports WebMIDI!');
+			} else {
+			    console.log('WebMIDI is not supported in this browser.');
+			}
+		</script>
+	</body>
+</html>
